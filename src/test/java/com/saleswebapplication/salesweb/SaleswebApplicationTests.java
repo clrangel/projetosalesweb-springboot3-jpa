4372c0983407c548af5f90f8e3dfcc89801139da
Camilo LR <autumndrums@hotmail.com>
2025-01-07 16:47:10 -0300

    Projeto criado

---
+++ src/test/java/com/saleswebapplication/salesweb/SaleswebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saleswebapplication.salesweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaleswebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
